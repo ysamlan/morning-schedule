da56e8f4687da65663838e125e60603388df289f
Your Name <ysamlan@gmail.com>
2025-02-28 13:09:12 -0500

    initial POC web worker stuff from an example
    * needs some major fixups

--- src/lib/types.ts
+++ src/lib/types.ts
@@ -22,3 +22,11 @@ export const DEFAULT_APP_STATE: AppState = {
     isSetupMode: true,
     alertTimes: []
 };
+
+
+export const STATUS_FINISHED = 'STATUS_FINISHED';
+export const STATUS_PROCESSING = 'STATUS_PROCESSING';
+export const STATUS_IDLE = 'STATUS_IDLE';
+export const STATUS_ERROR = 'STATUS_ERROR';
+export const MESSAGE_START = 'START';
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -9,6 +9,8 @@
     import AlertTimeBlock from '../components/AlertTimeBlock.svelte';
     import ClearDataButton from '../components/ClearDataButton.svelte';
     import Clock from '../components/Clock.svelte';
+    import { STATUS_FINISHED, STATUS_PROCESSING, STATUS_IDLE, MESSAGE_START } from '$lib/types';
+    import { browser } from '$app/environment';
 
     let alertTimes = localStore('alertTimes', [] as AlertTime[]);
     let isSetupMode = localStore('isSetupMode', true);
@@ -19,6 +21,103 @@
     let editingTimeId = '';
     let newItemName: { [key: string]: string } = {};
     let checkInterval: number;
+    let worker: Worker;
+
+    /**
+   * @type {Number} The delay each of the 100 loops in the worker has
+   */
+  let time = 300;
+  /**
+   * @type {Number} Counting the progress of the worker
+   */
+  let step = 0;
+  /**
+   * @type {Number} The total number of steps of the worker
+   */
+  let total = 0;
+  /**
+   * @type {Number|undefined} The end result the worker returns.
+   */
+  let result;
+  /**
+   * @type {String} The current status of the worker.
+   */
+  let workerStatus = STATUS_IDLE;
+  /**
+   * @type {String} Holds messages that are returend by the worker.
+   */
+  let workerMessage;
+
+    async function initWebWorker() {
+    // This function initiates the web worker
+    if (browser) {
+      // Check if we are in a browser
+      if (window.Worker) {
+        // Check if the browser supports web worker
+        // We reset some values we use to visualise the progress
+        workerStatus = STATUS_IDLE;
+        step = 0;
+        total = 0;
+        result = undefined;
+        // This is where we load the worker
+        const MyWorker = await import('$lib/workers/worker.js?worker');
+        // And initiate the worker
+        worker = new MyWorker.default();
+
+        // The following part is called when the worker sends a message
+        worker.onmessage = function (e) {
+          // Let’s first get the status and the message from the event’s data
+          const { status, message } = e.data;
+          // We use these two variables on the website
+          if (message) {
+            workerMessage = message;
+          }
+          if (status) {
+            workerStatus = status;
+          }
+          // This checks what the status of the message is
+          switch (status) {
+            case STATUS_FINISHED:
+              // Save the result returned from the web worker
+              result = e.data.result;
+              break;
+            case STATUS_PROCESSING:
+              // Save the current step number and total number of steps
+              step = e.data.step ?? 0; // Set to 0 instead of undefined if something went wrong
+              total = e.data.total ?? 0;
+              break;
+          }
+        };
+      }
+    }
+  }
+
+    async function startWorker() {
+    // This function first checks if any worker is present
+    if (worker) {
+      // If there is a worker currently running
+      if (workerStatus === STATUS_PROCESSING) {
+        // Stop the currently runnning worker
+        await stopWorker();
+      }
+      // Start the (new) task
+      worker.postMessage({ task: MESSAGE_START, time: time++ });
+    }
+  }
+
+  async function stopWorker() {
+    // This seams to be the least hacky way of stopping a worker.
+    // It would be nice to just cancel the function instead of terminating and reinitiating the worker.
+    terminateWorker();
+    await initWebWorker();
+  }
+
+  function terminateWorker() {
+    // Check if there is a worker present. This is sometimes nessecary when parts of the website reload.
+    if (worker) {
+      worker.terminate();
+    }
+  }
 
     function handleAddTime() {
         if (newTime) {
@@ -196,6 +295,7 @@
     onMount(() => {
         checkInterval = window.setInterval(checkTimes, 1000);
         
+        initWebWorker();
         // Initial preload of announcements
         if (!isSetupMode.value) {
             alertTimes.value.forEach(alertTime => {
@@ -208,6 +308,7 @@
         if (checkInterval) clearInterval(checkInterval);
         cancelSpeech();
         clearCache();
+        terminateWorker();
     });
 
     // Watch for changes to isSetupMode and rebuild audio cache when switching to checklist mode
@@ -216,7 +317,9 @@
         clearCache();
         alertTimes.value.forEach(alertTime => {
             preloadAnnouncement(alertTime).catch(console.error);
+            console.log(`Preloaded announcement for ${alertTime.time}`);
         });
+        console.log('Audio cache rebuilt');
     }
 </script>
 
@@ -255,6 +358,45 @@
     </div>
 
     <ModeToggle bind:isSetupMode={isSetupMode.value} />
+
+
+
+
+
+<!-- temporary web worker testing stuff -->
+<div class="flex w-full h-dvh items-center justify-center">
+    <div class="p-6 bg-gray-50 rounded flex flex-col gap-y-4 min-w-[500px] shadow-xl border border-gray-150">
+      <h1 class="text-xl font-bold">Web Worker in Sveltekit</h1>
+      <p class="max-w-prose text-sm">
+        This is a simple setup that demonstrates the use of <a class="underline decoration-blue-700" href="https://developer.mozilla.org/en-US/docs/Web/API/Web_Workers_API/Using_web_workers"
+          >web workers</a
+        > in Sveltekit.
+      </p>
+  
+      <dl class="grid grid-cols-2 items-center text-sm">
+        <dt>worker status</dt>
+        <dd><pre>{workerStatus}</pre></dd>
+        <dt>worker message</dt>
+        <dd><pre>{workerMessage}</pre></dd>
+        <dt>result</dt>
+        <dd><pre>{result}</pre></dd>
+      </dl>
+  
+      <progress class="accent-blue w-full bg-white border border-gray-200 rounded" max={total} value={step} />
+  
+      <div class="grid grid-cols-2 gap-x-2 w-full">
+        <button disabled={!Boolean(worker)} on:click={startWorker}>start worker</button>
+        <button disabled={Boolean(worker) && workerStatus !== STATUS_PROCESSING} on:click={stopWorker}>stop worker</button>
+      </div>
+      <footer>
+        <span class="text-sm"
+          >See the source code at <a class="underline decoration-blue-700" href="https://github.com/jnsprnw/webworker-sveltekit">https://github.com/jnsprnw/webworker-sveltekit</a>.</span
+        >
+      </footer>
+    </div>
+  </div>
+<!-- end temp web worker testing stuff -->  
+
 </main>
 
 <style lang="postcss">
